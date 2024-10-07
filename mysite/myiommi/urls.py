688a1ec8f128b268389ef9ec11d2b5389298ecf6
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 11:48:56 +0900

    新規作成フォームを追加

--- mysite/myiommi/urls.py
+++ mysite/myiommi/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from iommi import Form
+
+from .models import Album, Artist
 
 urlpatterns = [
+    path("albums/create", Form.create(auto__model=Album).as_view()),
+    path("artists/create", Form.create(auto__model=Artist).as_view()),
 ]
