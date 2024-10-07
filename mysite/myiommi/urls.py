dca9ed0448f452d2f52ed613ef4aef08c97d7a14
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 16:40:29 +0900

    フォーム送信後の遷移先を一覧ページに変更

--- mysite/myiommi/urls.py
+++ mysite/myiommi/urls.py
@@ -7,7 +7,7 @@ from .views import TopPage
 urlpatterns = [
     path("", TopPage().as_view()),
     path("albums/", Table(auto__model=Album).as_view()),
-    path("albums/create", Form.create(auto__model=Album).as_view()),
+    path("albums/create", Form.create(auto__model=Album, extra__redirect_to=".").as_view()),
     path("artists/", Table(auto__model=Artist).as_view()),
-    path("artists/create", Form.create(auto__model=Artist).as_view()),
+    path("artists/create", Form.create(auto__model=Artist, extra__redirect_to=".").as_view()),
 ]
