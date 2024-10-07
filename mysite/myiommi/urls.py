020d20dc3e85a1f3f934a4e5aa942842b1dd14a5
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 12:25:19 +0900

    テーブルの追加

--- mysite/myiommi/urls.py
+++ mysite/myiommi/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from iommi import Form
+from iommi import Form, Table
 
 from .models import Album, Artist
 
 urlpatterns = [
+    path("albums/", Table(auto__model=Album).as_view()),
     path("albums/create", Form.create(auto__model=Album).as_view()),
+    path("artists/", Table(auto__model=Artist).as_view()),
     path("artists/create", Form.create(auto__model=Artist).as_view()),
 ]
