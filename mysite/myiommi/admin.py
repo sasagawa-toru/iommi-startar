f5f8f99db36c659c4ca1967bd0f630caa57242ca
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 12:25:10 +0900

    表示調整

--- mysite/myiommi/admin.py
+++ mysite/myiommi/admin.py
@@ -3,13 +3,33 @@ from django.contrib import admin
 from .models import Album, Artist, Genre, Track
 
 
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "artist",
+        "year",
+    )
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+    )
+
 class GenreAdmin(admin.ModelAdmin):
     list_display = (
         "name",
     )
 
+class TrackAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "index",
+        "album",
+        "duration",
+    )
 
-admin.site.register(Album)
-admin.site.register(Artist)
+
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Genre, GenreAdmin)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
--- mysite/myiommi/models.py
+++ mysite/myiommi/models.py
@@ -11,9 +11,15 @@ from django.db.models import (
 class Artist(Model):
     name = CharField(max_length=255, db_index=True)
 
+    def __str__(self)->str:
+        return f"{self.name}"
+
 
 class Genre(Model):
     name = CharField(max_length=255, db_index=True)
+
+    def __str__(self)->str:
+        return f"{self.name}"
 
 
 class Album(Model):
@@ -22,9 +28,15 @@ class Album(Model):
     year = IntegerField(default=1980)
     genres = ManyToManyField(Genre, related_name="albums")
 
+    def __str__(self)->str:
+        return f"{self.name}"
+
 
 class Track(Model):
     name = CharField(max_length=255, db_index=True)
     index = IntegerField()
     album = ForeignKey(Album, on_delete=CASCADE, related_name="tracks")
     duration = CharField(max_length=255, db_index=False, null=True, blank=True)
+
+    def __str__(self)->str:
+        return f"{self.name}"
