253f3742368dc00dd2ce821d9a9e8d609acf4b77
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 11:48:48 +0900

    Django AdminのGenre一覧にジャンル名を表示

--- mysite/myiommi/admin.py
+++ mysite/myiommi/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 from .models import Album, Artist, Genre, Track
 
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+    )
+
+
 admin.site.register(Album)
 admin.site.register(Artist)
-admin.site.register(Genre)
+admin.site.register(Genre, GenreAdmin)
 admin.site.register(Track)
