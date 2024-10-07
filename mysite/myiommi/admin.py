074e5c1338dc598e1d3cbc75a31955d965b490c3
Sasagawa Toru <sasagawa.toru@nalgo.co.jp>
2024-10-07 10:01:19 +0900

    モデルを追加

--- mysite/myiommi/admin.py
+++ mysite/myiommi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album, Artist, Genre, Track
+
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Track)
--- mysite/myiommi/models.py
+++ mysite/myiommi/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db.models import (
+    CASCADE,
+    CharField,
+    ForeignKey,
+    IntegerField,
+    ManyToManyField,
+    Model,
+)
 
-# Create your models here.
+
+class Artist(Model):
+    name = CharField(max_length=255, db_index=True)
+
+
+class Genre(Model):
+    name = CharField(max_length=255, db_index=True)
+
+
+class Album(Model):
+    name = CharField(max_length=255, db_index=True)
+    artist = ForeignKey(Artist, on_delete=CASCADE, related_name="albums")
+    year = IntegerField(default=1980)
+    genres = ManyToManyField(Genre, related_name="albums")
+
+
+class Track(Model):
+    name = CharField(max_length=255, db_index=True)
+    index = IntegerField()
+    album = ForeignKey(Album, on_delete=CASCADE, related_name="tracks")
+    duration = CharField(max_length=255, db_index=False, null=True, blank=True)
